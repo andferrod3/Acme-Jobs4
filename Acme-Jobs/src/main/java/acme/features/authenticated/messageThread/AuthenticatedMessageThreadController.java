75ea786c5ac0c5d2d0072f4979d17bcf746764e5
Mario David <marjimhar@alum.us.es>
2019-11-29 21:58:30 +0100

    List 024: finished

---
+++ Acme-Jobs/src/main/java/acme/features/authenticated/messageThread/AuthenticatedMessageThreadController.java
@@ -0,0 +1,29 @@
+
+package acme.features.authenticated.messageThread;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import acme.components.CustomCommand;
+import acme.entities.messageThreads.MessageThread;
+import acme.framework.components.BasicCommand;
+import acme.framework.controllers.AbstractController;
+import acme.framework.entities.Authenticated;
+
+@Controller
+@RequestMapping("/authenticated/message-thread/")
+public class AuthenticatedMessageThreadController extends AbstractController<Authenticated, MessageThread> {
+
+	@Autowired
+	private AuthenticatedMessageThreadListMineService listService;
+
+
+	@PostConstruct
+	private void initialise() {
+		super.addCustomCommand(CustomCommand.LIST_MINE, BasicCommand.LIST, this.listService);
+	}
+
+}
