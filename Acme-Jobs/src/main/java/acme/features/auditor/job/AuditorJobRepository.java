90b05c81eb0f3131d8a4814e507c5f946942abef
carlos santos <carsantir@alum.us.es>
2019-11-30 17:34:40 +0100

    Task 029:finished

--- Acme-Jobs/src/main/java/acme/features/auditor/job/AuditorJobRepository.java
+++ Acme-Jobs/src/main/java/acme/features/auditor/job/AuditorJobRepository.java
@@ -12,6 +12,9 @@ import acme.framework.repositories.AbstractRepository;
 @Repository
 public interface AuditorJobRepository extends AbstractRepository {
 
+	@Query("select j from Job j where j.id=?1")
+	Job findOneJobById(int id);
+
 	@Query("select j from Job j join AuditRecord ar on ar.job.id=j.id where ar.auditor.id!=?1")
 	Collection<Job> findManyByEmployerId(int auditorId);
 }
