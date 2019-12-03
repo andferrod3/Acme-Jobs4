c5fbed5619e58623446c76e6431afd9fa213d287
carlos santos <carsantir@alum.us.es>
2019-12-03 11:21:36 +0100

    Fixing issues

--- Acme-Jobs/src/main/java/acme/features/auditor/notWrittenJob/AuditorNotWrittenJobRepository.java
+++ Acme-Jobs/src/main/java/acme/features/auditor/notWrittenJob/AuditorNotWrittenJobRepository.java
@@ -15,6 +15,6 @@ public interface AuditorNotWrittenJobRepository extends AbstractRepository {
 	@Query("select j from Job j where j.id=?1")
 	Job findOneJobById(int id);
 
-	@Query("select j from Job j join AuditRecord ar on ar.job.id=j.id where ar.auditor.id!=?1")
+	@Query("select j from Job j where j.id not in (select ar.job.id from AuditRecord ar where ar.auditor.id=55)")
 	Collection<Job> findManyByAuditorId(int auditorId);
 }
